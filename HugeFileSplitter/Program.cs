619ec6cbb5c40ea0d74fa448a1765b85bf63c5e4
Sertunc Selen <cahit.selen@nvi.gov.tr>
2023-08-03 10:08:48 +0300

    Add project files.

---
+++ HugeFileSplitter/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace HugeFileSplitter
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string folderPath = "D:\\updateFiles";
+            string largeFileName = "updateFile.upgrade";
+            int chunkSizeInBytes = 262144; // Örnek olarak her parça 256 KB boyutunda olacak
+
+            string filePath = Path.Combine(folderPath, largeFileName);
+            byte[] data = File.ReadAllBytes(filePath);
+
+            for (int i = 0; i < data.Length; i += chunkSizeInBytes)
+            {
+                int chunkSize = Math.Min(chunkSizeInBytes, data.Length - i);
+                byte[] chunk = new byte[chunkSize];
+                Array.Copy(data, i, chunk, 0, chunkSize);
+
+                string chunkFileName = $"chunk_{i / chunkSizeInBytes}.upgrade";
+                string chunkFilePath = Path.Combine(folderPath, chunkFileName);
+
+                File.WriteAllBytes(chunkFilePath, chunk);
+            }
+
+            Console.WriteLine("Dosya parçalama işlemi tamamlandı!");
+
+            Console.ReadLine();
+        }
+    }
+}
