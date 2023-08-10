139ad8998aed2d8bac77de2195d4fdcfa86af05c
Sertunc Selen <cahit.selen@nvi.gov.tr>
2023-08-10 15:25:47 +0300

    add different using about filestream

--- HugeFileSplitter/Program.cs
+++ HugeFileSplitter/Program.cs
@@ -1,4 +1,8 @@
-﻿using BenchmarkDotNet.Running;
+﻿using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Buffers;
 
 namespace HugeFileSplitter
 {
@@ -26,25 +30,77 @@ namespace HugeFileSplitter
             //    File.WriteAllBytes(chunkFilePath, chunk);
             //}
 
-            int i = 0;
-            using (FileStream fs = File.OpenRead(filePath))
+            //int i = 0;
+            //using (FileStream fs = File.OpenRead(filePath))
+            //{
+            //    byte[] buffer = new byte[chunkSizeInBytes];
+            //    int bytesRead;
+
+            //    while ((bytesRead = fs.Read(buffer, 0, chunkSizeInBytes)) > 0)
+            //    {
+            //        if (bytesRead == buffer.Length)
+            //        {
+            //            Console.WriteLine($"Read {bytesRead} {buffer.Length} bytes {i}");
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine($"Read {bytesRead} {buffer.Take(bytesRead).ToArray().Length} bytes {i}");
+            //        }
+
+            //        i++;
+            //    }
+            //}
+
+            //var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+            //var blockCount = stream.Length > chunkSizeInBytes ? (uint)Math.Ceiling((decimal)stream.Length / (decimal)chunkSizeInBytes) : 1;
+
+            //var arrayPool = ArrayPool<byte>.Shared;
+            //var buffer = arrayPool.Rent(chunkSizeInBytes);
+
+            //try
+            //{
+            //    for (uint i = 0; i < blockCount; i++)
+            //    {
+            //        var bytesRead = stream.Read(buffer, 0, buffer.Length);
+
+            //        if (bytesRead == buffer.Length)
+            //            Console.WriteLine($"Read {bytesRead} {buffer.Length} bytes {i}");
+            //        else
+            //            Console.WriteLine($"Read {bytesRead} {buffer.Take(bytesRead).ToArray().Length} bytes {i}");
+            //    }
+            //}
+            //finally
+            //{
+            //    arrayPool.Return(buffer, false);
+            //}
+
+            //int i = 0;
+            //using (FileStream fs = File.OpenRead(filePath))
+            //{
+            //    byte[] buffer = new byte[chunkSizeInBytes];
+
+            //    for (int startingIndex = 0; startingIndex < fs.Length; startingIndex += chunkSizeInBytes)
+            //    {
+            //        int bytesRead = fs.Read(buffer, 0, chunkSizeInBytes);
+
+            //        Console.WriteLine($"Read {bytesRead} bytes from index {startingIndex} {i}");
+            //        i++;
+            //    }
+            //}
+
+            long startingPosition = 2;
+            int bytesToRead = 3;
+
+            using (FileStream fs = File.OpenRead("D:\\KecUpdate\\MilkoKec\\2.6\\00002735\\file.txt"))
             {
-                byte[] buffer = new byte[chunkSizeInBytes];
-                int bytesRead;
+                fs.Seek(startingPosition, SeekOrigin.Begin); // Konumu ayarla
 
-                while ((bytesRead = fs.Read(buffer, 0, chunkSizeInBytes)) > 0)
-                {
-                    if (bytesRead == buffer.Length)
-                    {
-                        Console.WriteLine($"Read {bytesRead} {buffer.Length} bytes {i}");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Read {bytesRead} {buffer.Take(bytesRead).ToArray().Length} bytes {i}");
-                    }
+                byte[] buffer = new byte[bytesToRead];
+                int bytesRead = fs.Read(buffer, 0, bytesToRead);
 
-                    i++;
-                }
+                char[] cArray = System.Text.Encoding.UTF8.GetString(buffer).ToCharArray();
+
+                Console.WriteLine($"Read {bytesRead} bytes from position {startingPosition}");
             }
 
             Console.WriteLine("Dosya parçalama işlemi tamamlandı!");
